4e64e3109074b59b4940e93d4a4fc471e8c0030b
Yago Garcez <yago.garcez@outlook.com>
2021-06-18 21:46:24 -0300

    Desafio Concluído

---
+++ src/components/Features/index.tsx
@@ -0,0 +1,36 @@
+import { Grid, GridItem } from "@chakra-ui/react";
+import Feature from "./Feature";
+
+export default function Features() {
+    return (
+        <Grid
+            templateColumns={['1fr 1fr', '1fr 1fr', '1fr 1fr', 'repeat(5, 1fr)']}
+            w='100%'
+            justifyContent='space-between'
+            align='center' mt={['10', '32']}
+            mx='auto'
+            maxW='1160px'
+            gap={[1, 5]}
+        >
+            <GridItem>
+                <Feature icon='cocktail' text='vida noturna' />
+            </GridItem>
+
+            <GridItem>
+                <Feature icon='surf' text='praia' />
+            </GridItem>
+
+            <GridItem>
+                <Feature icon='building' text='moderna' />
+            </GridItem>
+
+            <GridItem>
+                <Feature icon='museum' text='clássico' />
+            </GridItem>
+
+            <GridItem colSpan={[2, 2, 2, 1]}>
+                <Feature icon='earth' text='e mais...' />
+            </GridItem>
+        </Grid>
+    );
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,28 @@
+import { Flex, Grid, Icon, Image } from "@chakra-ui/react";
+import { useRouter } from "next/dist/client/router";
+import Link from 'next/link';
+import { RiArrowLeftSLine } from 'react-icons/ri';
+
+export default function Header() {
+    const { asPath } = useRouter();
+    const notHomePage = asPath != '/';
+
+
+    return (
+        <Flex bg='white' w='100%' as='header' mx='auto' px='1rem' h={['50px', '100px']} align='center' justify='center'>
+            <Grid h='100%' mx='auto' w='100%' maxW='1160px' alignItems='center' templateColumns='repeat(3, 1fr)' justifyContent='center'>
+                {notHomePage && (
+                    <Link href='/'>
+                        <a>
+                            <Icon as={RiArrowLeftSLine} fontSize={[20, 40]} justifySelf='start' />
+                        </a>
+                    </Link>
+                )}
+
+
+                <Image w={['81px', '184px']} src='/logo.svg' alt='Um avião voando sobre o logo da marca World Trip' justifySelf='center' gridColumn='2' />
+            </Grid>
+
+        </Flex>
+    );
+}
